1942eb76dcd0e053e9d34ba76cc3aea562b18ccf
jasomorg <jasomorg@qti.qualcomm.com>
2023-03-14 21:07:09 +0000

    ignore certs file as it's auto generated Abandon the serial/ip and make secure websockets work

--- mqtt/mqtt.ino
+++ mqtt/mqtt.ino
@@ -2,11 +2,25 @@
 #define USE_WIFI_NETWORK
 #define USE_WEBSOCKETS
 #define USE_SSL
+#undef USE_MQTT_PUBLISHER
+#undef USE_HTTP_ERR_PRETTY
 
 #include <ESP8266HTTPClient.h>
 #include <PubSubClient.h>
+#include <PubSubClientTools.h>
+#include <string>
+#include <stdexcept>
+	
+
+#ifdef USE_MQTT_PUBLISHER
+#include <Thread.h>
+#include <ThreadController.h>
+#endif
+
 #include "secrets.h"
 
+// Ensure the compiler does not get the storage wrong
+// for strings as otherwise they take up a lot of our 4k stack
 #define ROMSTR(v,s) const typeof(*s) * v PROGMEM = s
 
 #ifndef USE_WIFI_NETWORK
@@ -36,6 +50,8 @@ ROMSTR(msg_connected, "Connected\r\n");
 ROMSTR(msg_crlf, "\r\n");
 ROMSTR(msg_null, "");
 
+
+#ifdef USE_HTTP_ERR_PRETTY
 const char * http_error_str [] PROGMEM = {
   "SUCCESS",
   "CONNECTION FAILED",
@@ -43,8 +59,8 @@ const char * http_error_str [] PROGMEM = {
   "TIMED OUT",
   "INVALID RESPONSE",
 };
-
 const int http_error_count = sizeof(http_error_str)/sizeof(const char *);
+#endif
 
 #ifdef USE_WIFI_NETWORK
 #define MSG(x) Serial.write(x)
@@ -83,16 +99,17 @@ This reads .\secrets.h and creates .\certs.h to match
 
 #ifdef USE_WIFI_NETWORK
 #ifdef USE_SSL
-WiFiClientSecure net;
+WiFiClientSecure netClient;
 #else
-WiFiClient net;
+WiFiClient netClient;
 #endif
 #define SERIAL_BAUD 115200
 #else
-ArduinoSerialToTCPBridgeClient net;
+ArduinoSerialToTCPBridgeClient netClient;
 #define SERIAL_BAUD 921600
 #endif
 
+#ifdef USE_HTTP_ERR_PRETTY
 const char * get_http_error(int error) {
   if (error > 0 || error <= -http_error_count) {
     Serial_printf("Unknown HTTP err: %d\n", error);
@@ -100,10 +117,17 @@ const char * get_http_error(int error) {
   }
   return http_error_str[-error];
 }
+#endif
 
-WebSocketClient ws(net, myhost_fqdn, myhost_port);
-WebSocketStreamClient ws_client(ws, "/mqtt");
-PubSubClient mqttClient(ws_client);
+WebSocketClient wsClient(netClient, myhost_fqdn, myhost_port);
+WebSocketStreamClient wsStream(wsClient, "/mqtt");
+PubSubClient mqttClient(wsStream);
+PubSubClientTools mqtt(mqttClient);
+
+#ifdef USE_MQTT_PUBLISHER
+ThreadController threadControl();
+Thread thread();
+#endif
 
 #define pin_onoff_prod 4
 #define pin_onoff_preprod 5
@@ -115,77 +139,90 @@ X509List cert(Root_CA);
 static unsigned long long next_connect = 0LL;
 static unsigned short int pulse_ms = 100;
 static bool onoff[2] = {false, false};
-static bool last_pulse_onoff [2] = {false, false};
 static unsigned long long next_pulse_evt_ms [2] = {0LL, 0LL};
 
-void callback_slider(double x) {
-  if ((x * 1000) > 65535) x = 65535.0;
-  if (x < 0) x = 0.0;
-  pulse_ms = (unsigned short)(x * 1000.0); 
+// Helper to parse the REST like MQTT message for the channel number
+int get_chan(const std::string & text) {
+  const char * value = text.substr(text.rfind('/')+1).c_str();
+  if (*value != '0' && *value != '1')return -1;
+  int chan = ((signed char)(*value)) - '0';
+  return chan;
 }
 
-void handle_onoff(int chan, char *data) {
+// Called by MQTT
+void callback_pulse_ms(const std::string & topic, const std::string & msg) {
+  (void)topic;
+  double value = 0.0;
+  try {
+    value = std::stof(msg.c_str());
+  } catch (std::logic_error const& ex) {
+    Serial_printf("Bad Message: %s\n", msg.c_str());
+    return;
+  }
+  if ((value * 1000) > 65535) value = 65535.0;
+  if (value < 0) value = 0.0;
+  pulse_ms = (unsigned short)(value * 1000.0); 
+}
+
+// Handle the messages for onoff
+void handle_onoff(int chan, const std::string & msg) {
   char ch;
-  signed char val = data[0] - '0';
+  signed char val = msg[0] - '0';
   if (chan < 0 || chan > 1) return;
   if (val < 0 || val > 1) return;
-
+  bool prev = onoff[chan];
   onoff[chan] = val==1?true:false;
 
-  ch = '0' + chan;
+  // Nothing to do
+  if (onoff[chan] == prev)return;
 
-  MSG("Onoff updated on chan ");
-  Serial_print(ch);
-  MSG(" Now ");
-  MSG(val?"On":"Off");
-  MSG_CRLF;
-
+  Serial_printf("Relay chan %d -> %s\n", chan, val?"On":"Off");
 }
 
-void callback_onoff0(char *data, uint16_t len) {
-  handle_onoff(0, data);
-}
-
-void callback_onoff1(char *data, uint16_t len) {
-  handle_onoff(1, data);
-}
-
-
-void handle_pulse(int chan) {
+// Requesting a pulse either makes a pulse now (on->off-> or off->on->off)
+// or extends an existing pulse
+void handle_pulse(int chan, const std::string & msg) {
   char ch;
   unsigned long long now = millis();
   unsigned long long next = now + pulse_ms;
+  bool pulse_active = next_pulse_evt_ms[chan] != 0L;
+
   if (pulse_ms == 0L) return;
   if (chan < 0 || chan > 1) return;
 
-  last_pulse_onoff[chan] = onoff[chan];
-
+  // Handle overflows as millis() wrap every 50 days or so
   if (next > 0xFFFFFFFFL) next -= 0xFFFFFFFFL;
 
+  // If the result is next == 0LL then make it 1 as
+  // zero is used for never.
+  if (next == 0LL) next= 1LL;
+
   next_pulse_evt_ms[chan] = (unsigned long)next;
+  if (! pulse_active ) onoff[chan] = ! onoff[chan];
   
-  ch = '0' + chan;
-
-  MSG("Pulse ");
-  Serial_print(pulse_ms);
-  MSG(" requested on: chan ");
-  Serial_print(ch);
-  MSG_CRLF;
-
+  Serial_printf("%3.4fs pulse triggered on chan %d\r\n", pulse_ms / 1000.0, chan);
 }
 
-void callback_pulse0(char *data, uint16_t len) {
-  (void)data;
-  (void)len;
-  handle_pulse(0);
+
+
+
+// Called by MQTT
+void callback_onoff(const std::string & topic, const std::string & msg) {
+  int chan = get_chan(topic);
+  if (chan < 0)return;
+  
+  handle_onoff(chan, msg);
 }
 
-void callback_pulse1(char *data, uint16_t len) {
-  (void)data;
-  (void)len;
-  handle_pulse(1);
+void callback_pulse(const std::string & topic, const std::string & msg) {
+  int chan = get_chan(topic);
+  if (chan < 0)return;
+  handle_pulse(chan, msg);
 }
 
+
+
+// Setup the IO pins for the relays
 void setupIO()
 {
   pinMode(pin_onoff_preprod, OUTPUT);
@@ -195,6 +232,8 @@ void setupIO()
 }
 
 
+// This makes the initial MQTT connection
+// by upgrading a HTTPS session to a WSS session
 ROMSTR(uid, "QDEFSYS");
 int mqttConnect() {
   wdt_reset();
@@ -209,15 +248,17 @@ int mqttConnect() {
     return MQTT_CONNECTED;
   }
   MSG("Connecting MQTT:\r\n");
-  mqttClient.setCallback(MQTTcallback);
+  //mqttClient.setCallback(MQTTcallback);
   bool connected = mqttClient.connect(uid, MQTT_USERNAME, MQTT_KEY);
   if (connected) {
     MSG_CONNECTED;
-    mqttClient.subscribe("test");
+    mqtt.subscribe(F("cfg/pulse_ms"), MAKE_CALLBACK(callback_pulse_ms));
+    mqtt.subscribe(F("dev/onoff/0"),  MAKE_CALLBACK(callback_onoff));
+    mqtt.subscribe(F("dev/onoff/1"),  MAKE_CALLBACK(callback_onoff));
+    mqtt.subscribe(F("dev/pulse/0"),  MAKE_CALLBACK(callback_pulse));
+    mqtt.subscribe(F("dev/pulse/1"),  MAKE_CALLBACK(callback_pulse));
   } else {
-    MSG("Fail: HTTP State: ");
-    int http_status = ws.responseStatusCode();
-    Serial_printf("%d: %s\n", http_status, get_http_error(http_status));
+    MSG("Connect Fail\r\n");
   }
   wdt_reset();
 
@@ -230,6 +271,8 @@ int mqttConnect() {
   return current_state;
 }
 
+// Use this for longer delays to prevent the watchdog
+// kicking in
 void delay_s(unsigned long ms) {
   unsigned long count = ms / 100;
   for ( ; count > 0; count--) {
@@ -238,6 +281,7 @@ void delay_s(unsigned long ms) {
   }
 }
 
+// Connect to NTP
 ROMSTR(ntp_server0, "pool.ntp.org");
 ROMSTR(ntp_server1, "time.nist.gov");
 void setupNTP() {
@@ -258,7 +302,7 @@ void setupNTP() {
   Serial.print(asctime(&timeinfo));
 }
 
-
+// Connect to WIFI
 void setupWiFi() {
 
   MSG_CRLF;
@@ -284,10 +328,11 @@ void setupWiFi() {
 
 
 void setupHost() {
-  Serial.printf("Connecting to %s:%u\r\n",myhost_fqdn,myhost_port);
+  Serial.printf("Connecting to %s:%u\r\n", myhost_fqdn, myhost_port);
+
 #ifdef USE_SSL
-  //Serial.printf("Using certificate: %s\n", Root_CA);
-  net.setTrustAnchors(& cert);
+  Serial.printf("Using certificate: %s\n", Root_CA_cn);
+  netClient.setTrustAnchors(& cert);
 #endif
 }
 
@@ -299,43 +344,70 @@ void setup() {
   Serial.begin(SERIAL_BAUD);
   delay_s(5000);
   MSG_CRLF;
+
+  MSG("Reset Reason:\r\n");
   Serial.println(ESP.getResetReason());
   Serial.println(ESP.getResetInfo());
 
   MSG("\n\nRemote USB Unplug over MQTT\r\n");
 
-
 #ifdef USE_WIFI_NETWORK
   setupWiFi();
 #endif
 
+  // NTP is required for https / wss
   setupNTP();
   setupHost();
 
-
+#ifdef USE_MQTT_PUBLISHER
+  // Enable Thread
+  thread.onRun(publisher);
+  thread.setInterval(2000);
+  threadControl.add(&thread);
+#endif
 }
 
+// Update the relay states
 void relayLoop() {
   for(unsigned int chan=0;chan<1;chan++) {
-    if (next_pulse_evt_ms[chan] < millis()){
-      digitalWrite(pin_onoff_preprod, onoff[chan]);
-    } else {
-      digitalWrite(pin_onoff_preprod, ~last_pulse_onoff[chan]);
+    if (millis() > next_pulse_evt_ms[chan] & next_pulse_evt_ms[chan] !=0L) {
+      onoff[chan] = !onoff[chan];
+      next_pulse_evt_ms[chan] = 0L;
     }
+    digitalWrite(pin_onoff_preprod, onoff[chan]);
   }
 }
 
 // Main work loop
 void loop() {
 
+  // Keep connecting, the underlying system handles
+  // actual connections as required
   mqttConnect();
 
+  // Keep processing MQTT messages
   mqttClient.loop();
-  //relayLoop()
+
+  // Keep updating the relay
+  relayLoop();
+
+  //  Feed the dog, just in case
   wdt_reset();
+#ifdef USE_MQTT_PUBLISHER
+  // Publish data on a thread
+  threadControl.run();
+#endif
 }
 
+#ifdef USE_MQTT_PUBLISHER
+void publisher() {
+  ++value;
+  mqtt.publish("test_out/hello_world", s+"Hello World! - No. "+value);
+}
+#endif
 
+/*
+// Callback used if we are not using the PubSubClientTools
 void MQTTcallback(char *topic, byte *payload, unsigned int length)
 {
     Serial_printf("Topic: %s : Payload: \r\n", topic);
@@ -343,3 +415,4 @@ void MQTTcallback(char *topic, byte *payload, unsigned int length)
         Serial_print((char)payload[i]);
     MSG_CRLF;
 }
+*/
