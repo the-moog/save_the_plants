8f8d4ba1a10ea4fbf72a6133a6508f063e0e3ca7
jasomorg <jasomorg@qti.qualcomm.com>
2023-03-15 01:52:58 +0000

    Wrote code to capture certificates from host and code to connect to the host in python over websockets

--- cert.py
+++ cert.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-# Script to download/update certificates and public keys
-# and generate compilable source files for c++/Arduino.
-# released to public domain
 
 import urllib.request
 import re
@@ -11,6 +8,7 @@ import sys
 import socket
 import argparse
 import datetime
+from warnings import WarningMessage
 
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
@@ -18,96 +16,254 @@ from cryptography.hazmat.primitives.serialization import pkcs7
 from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.hazmat.primitives.serialization import PublicFormat
 
-def printData(data, showPub = True, symbol=None):
-    try:
-        xcert = x509.load_der_x509_certificate(data)
-    except:
+def get_dt() -> str:
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Certificate:
+
+    def __init__(self, cert_data: str, clang_symbol: str|None = None):
+        self._cert_data = cert_data
+        self._symbol = clang_symbol
+        self._cert = None
+
+    def set_symbol(self, symbol: str|None):
+        self._symbol = symbol
+
+    @property
+    def cert(self):
+        # Cache the result so it's faster each access
+        if self._cert is None:
+            self.decode_cert(self._cert_data)
+        return self._cert
+
+    def decode_cert(self):
+        """
+        Try and decode cert_data into either an x509 or pkcs7 certificate
+        """
+        self._cert = None
         try:
-            xcert = x509.load_pem_x509_certificate(data)
+            self._cert = x509.load_der_x509_certificate(self._cert_data)
         except:
             try:
-                xcert = pkcs7.load_der_pkcs7_certificates(data)
+                self._cert = x509.load_pem_x509_certificate(self._cert_data)
             except:
-                xcert = pkcs7.load_pem_pkcs7_certificates(data)
-            if len(xcert) > 1:
-                print('// Warning: TODO: pkcs7 has {} entries'.format(len(xcert)))
-            xcert = xcert[0]
+                try:
+                    self._cert = pkcs7.load_der_pkcs7_certificates(self._cert_data)
+                except:
+                    self._cert = pkcs7.load_pem_pkcs7_certificates(self._cert_data)
+                if len(self._cert) > 1:
+                    WarningMessage(f'// Warning: TODO: pkcs7 has {len(self._cert)} entries')
 
-    cn = ''
-    for dn in xcert.subject.rfc4514_string().split(','):
-        keyval = dn.split('=')
-        if keyval[0] == 'CN':
-            cn += keyval[1]
-
-    name = re.sub('[^a-zA-Z0-9_]', '_', cn)
-    if symbol is None:
-        symbol = name
-
-    print('// CN: {} => name: {}'.format(cn, name))
-
-
-    print('// not valid before:', xcert.not_valid_before)
-    print('// not valid after: ', xcert.not_valid_after)
-
-    if showPub:
-
-        fingerprint = xcert.fingerprint(hashes.SHA1()).hex(':')
-        print('const char fingerprint_{} [] PROGMEM = "{}";'.format(name, fingerprint))
-
-        pem = xcert.public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode('utf-8')
-        print('const char pubkey_{} [] PROGMEM = R"PUBKEY('.format(name))
-        print(pem + ')PUBKEY";')
+    @property
+    def ca_list(self):
+        """
+        Get a list of CAs that have signed this certificate
+        """   
+        cas = []
+        if self.cert is not None:
+            for ext in self.cert.extensions:
+                if ext.oid == x509.ObjectIdentifier("1.3.6.1.5.5.7.1.1"):
+                    for desc in ext.value:
+                        if desc.access_method == x509.oid.AuthorityInformationAccessOID.CA_ISSUERS:
+                            cas.append(desc.access_location.value)
+        return cas  
     
-    else:
+    @property
+    def cn(self):
+        """
+        Extract the common name (CN) from distinguished name (DN)
+        """
+        cn = ''
+        for dn in self.cert.subject.rfc4514_string().split(','):
+            keyval = dn.split('=')
+            if keyval[0] == 'CN':
+                cn += keyval[1]
+        return cn
+    
+    @property
+    def clang_name(self):
+        """
+        Make a valid C name
+        """
+        return re.sub('[^a-zA-Z0-9_]', '_', self.cn)
 
-        cert = xcert.public_bytes(Encoding.PEM).decode('utf-8')
-        print('const char cert_{} [] PROGMEM = R"CERT('.format(name))
-        print(cert + ')CERT";')
-        print('const char cert_cn_{} [] PROGMEM = "{}";'.format(name, cn))
+    @property
+    def clang_symbol(self):
+        if self._symbol is None:
+            return self.clang_name
+        return self._symbol
 
-    cas = []
-    for ext in xcert.extensions:
-        if ext.oid == x509.ObjectIdentifier("1.3.6.1.5.5.7.1.1"):
-            for desc in ext.value:
-                if desc.access_method == x509.oid.AuthorityInformationAccessOID.CA_ISSUERS:
-                    cas.append(desc.access_location.value)
-    #print('const char * const {}_cert = cert_{};'.format(symbol, name))
-    if showPub:
-        print('const char * const {}_fingerprint = fingerprint_{};'.format(symbol, name))
-        print('const char * const {}_pubkey = pubkey_{};'.format(symbol, name))
+    @property
+    def fingerPrint(self):
+        return self.cert.fingerprint(hashes.SHA1()).hex(':')
+    
+    @property
+    def pubKey_pem(self):
+        return self.cert.public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode('utf-8')
 
-    for ca in cas:
-        with urllib.request.urlopen(ca) as crt:
-            print()
-            print('// ' + ca)
-            symbol = printData(crt.read(), False)
-        print()
-    return symbol
+    @property
+    def clang_fingerPrint(self):
+        return f'const char fingerprint_{self.clang_name} [] PROGMEM = "{self.fingerPrint}";'
 
-def get_certificate(hostname, port, name):
-    context = ssl.create_default_context()
-    #ip = socket.gethostbyname(hostname)
-    #pem = ssl.get_server_certificate((ip, port))
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
-    with socket.create_connection((hostname, port)) as sock:
-        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-            pem = ssock.getpeercert(binary_form=True)
-    print('////////////////////////////////////////////////////////////')
-    print(f'// certificate chain for {hostname}:{port}')
-    print()
-    if name:
-        print(f'const char * {name}_fqdn = "{hostname}";')
-        print(f'const uint16_t {name}_port = {port};')
-        print()
-    symbol = printData(pem, symbol=name)
-    print(f'const char * const Root_CA = cert_{symbol};')
-    print(f'const char * const Root_CA_cn = cert_cn_{symbol};')
-    print(f'// end of certificate chain for {hostname}:{port}')
-    print('////////////////////////////////////////////////////////////')
-    print()
-    return 0
+    @property
+    def clang_pubKey(self):
+        lines = []
+        lines.append(f'const char pubkey_{self.clang_name} [] PROGMEM = R"PUBKEY(')
+        lines.append(self.pubKey_pem + ')PUBKEY";')
+        return lines.join("\n")
 
+    @property
+    def cert_pem(self):
+        return self.cert.public_bytes(Encoding.PEM).decode('utf-8')
+
+    @property
+    def clang_pem(self):
+        lines = []
+        lines.append(f'const char cert_{self.clang_name} [] PROGMEM = R"CERT(')
+        lines.append(self.cert_pem + ')CERT";')
+        lines.append(f'const char cert_cn_{self.clang_name} [] PROGMEM = "{self.cn}";')
+
+    @property
+    def not_valid_before(self):
+        return self.cert.not_valid_before
+    
+    @property
+    def not_valid_after(self):
+        return self.cert.not_valid_after
+
+
+class CertChain:
+
+    def __init__(self, hostname: str, port: int|str = 443, name: str|None = None):
+        """
+        Get the certificate chain for https://<host>:<port> and (optionally)
+        use <name> to describe it
+        """
+        self._host = hostname
+        self._port = int(port)
+        self._name = name
+        self._chain = dict()
+        self._root = None
+
+    def get_chain(self, data: str, showPub: bool = False, symbol: str|None = None, clang: bool = False) -> list[str]:
+        cert = Certificate(data, clang_symbol=symbol)
+        self._chain[hash(data)] = cert
+        self._root = cert
+
+        lines = []
+        if clang:
+            append = lines.append
+        else:
+            append = lambda x : None
+
+        append(f'// CN: {cert.cn} => name: {cert.clang_name}')
+        append('// not valid before:', cert.not_valid_before)
+        append('// not valid after: ', cert.not_valid_after)
+
+        if showPub:
+            append(cert.clang_fingerPrint)
+            append(cert.clang_pubKey)
+        else:
+            append(cert.clang_pem)
+
+        if showPub:
+            append(f'const char * const {cert.clang_symbol}_fingerprint = fingerprint_{cert.clang_name};')
+            append(f'const char * const {cert.clang_symbol}_pubkey = pubkey_{cert.clang_name};')
+
+        for ca in cert.ca_list:
+            with urllib.request.urlopen(ca) as crt:
+                append("")
+                append('// ' + ca)
+                # Set root to the most recent cert as the last one is the root for this chain
+                self._root = cert
+                self.get_chain(crt.read(), showPub=False, clang=clang)
+            append("")
+        
+        return lines
+    
+    @property
+    def ca_symbol(self) -> str:
+        if self._root is None:
+            return "INVALID_ROOT"
+        return self._root.clang_symbol
+    
+    @staticmethod
+    def fetch_pem(host: str, port: int|str) -> str:
+        """
+        Get a PEM cert for a given host
+        """
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        with socket.create_connection((host, int(port))) as sock:
+            with context.wrap_socket(sock, server_hostname=host) as ssock:
+                pem = ssock.getpeercert(binary_form=True)
+        return pem
+
+    @property
+    def pem(self):
+        """
+        Return the PEM format certificate for the host we are interested in
+        """
+        return self.fetch_pem(self._host, self._port)
+
+    def get_clang_cert(self, include_url: bool = True) -> list[str]:
+        """
+        Get the PEM certificate and encode it into valid C syntax
+        """
+        lines = []
+        self.ca_symbol = None
+
+        # Chain header
+        lines.append('////////////////////////////////////////////////////////////')
+        lines.append(f'// certificate chain for {self._host}:{self._port}')
+        lines.append("")
+        if include_url:
+            lines.append(f'const char * {self._name}_fqdn = "{self._host}";')
+            lines.append(f'const uint16_t {self._name}_port = {self._port};')
+            lines.append("")
+
+        # Chain data (recursive)
+        lines.extend(self.get_chain(self.pem, symbol=self._name, clang=True))
+
+        # Chain footer
+        lines.append(f'const char * const Root_CA = cert_{self.ca_symbol};')
+        lines.append(f'const char * const Root_CA_cn = cert_cn_{self.ca_symbol};')
+        lines.append(f'// end of certificate chain for {self._host}:{self._port}')
+        lines.append('////////////////////////////////////////////////////////////')
+        lines.append("")
+        return lines
+    
+    @staticmethod
+    def get_clang_header() -> list[str]:
+        lines = []
+        lines.append("")
+        lines.append('// this file is autogenerated - any modification will be overwritten')
+        lines.append('// unused symbols will not be linked in the final binary')
+        lines.append(f'// generated on {get_dt()}')
+        lines.append(f'// by {sys.argv}')
+        lines.append("")
+        lines.append('#pragma once')
+        lines.append("")
+        return lines
+    
+    def cert_as_clang(self) -> str:
+        lines = []
+        lines.extend(self.get_clang_header())
+        lines.extend(self.get_clang_cert())
+        return "\n".join(lines)
+    
+    def get_ca_pem(self) -> str:
+        self.get_chain(self.pem, symbol=self._name, clang=True)
+        return self._root.cert_pem
+
+# External mondule interface
+def get_ca_pem(host: str, port: int|str = 443, name: str|None = None) -> str:
+    chain = CertChain(host, port, name)
+    return chain.get_ca_pem()
+
+# CLI Interface
 def main():
     parser = argparse.ArgumentParser(description='download certificate chain and public keys under a C++/Arduino compilable form')
     parser.add_argument('-s', '--server', dest="server", action='store', required=True, help='TLS server dns name')
@@ -126,16 +282,10 @@ def main():
         port = args.port
     except:
         pass
-
-    print()
-    print('// this file is autogenerated - any modification will be overwritten')
-    print('// unused symbols will not be linked in the final binary')
-    print('// generated on {}'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-    print('// by {}'.format(sys.argv))
-    print()
-    print('#pragma once')
-    print()
-    return get_certificate(server, port, args.name)
+    
+    chain = CertChain(server, port, args.name)
+    print(chain.cert_as_clang())
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
--- mqtt/mqtt_client.py
+++ mqtt/mqtt_client.py
@@ -2,72 +2,115 @@ import random
 import sys
 import time
 from h_parser import get_c_defines
-from Adafruit_IO import MQTTClient
+from cert import get_ca_pem
+import random
+import threading
+
+from paho.mqtt import client as mqtt_client
 
 
 #  Defined in secrets.h
-defines = get_c_defines(("AIO_KEY", "AIO_USERNAME"), "secrets.h")
-AIO_KEY = defines['AIO_KEY']
-AIO_USERNAME = defines['AIO_USERNAME']
+defines = get_c_defines(("MQTT_HOST", "MQTT_PORT", "MQTT_USERNAME", "MQTT_KEY"), "secrets.h")
+MQTT_KEY = defines['MQTT_KEY']
+MQTT_USERNAME = defines['MQTT_USERNAME']
+MQTT_BROKER = defines["MQTT_HOST"]
+MQTT_PORT = defines["MQTT_PORT"]
+CLIENT_ID = "devsys-mqtt-py-{id}".format(id=random.randint(0, 1000))
 
 feeds = {
-    "SECONDS":f'time/seconds', 
-    "ONOFF0": f'{AIO_USERNAME}/feeds/onoff0',
-    "ONOFF1": f'{AIO_USERNAME}/feeds/onoff1',
-    "PULSE_MS": f'{AIO_USERNAME}/feeds/pulse_ms',
-    "PULSE0": f'{AIO_USERNAME}/feeds/pulse0',
-    "PULSE1": f'{AIO_USERNAME}/feeds/pulse1'
+    "ONOFF0": f'dev/onoff0',        # /dev/onoff<ch> <0|1>  ch=0|1, 1=on 0=off
+    "ONOFF1": f'dev/onoff1',
+    "PULSE_MS": f'cfg/pulse_ms',    # Pulse duration in milliseconds (float)
+    "PULSE0": f'dev/pulse0',        # /dev/pulse<ch>   ch=0|1 - Invert on/off state for cfg/pulse_ms
+    "PULSE1": f'dev/pulse1'
 }
 
-def connected(client):
-    print('Connected to Adafruit IO!  Subscribing')
-    for feed, path in feeds.items():
-        print(f"Subscribing to {feed} on {path}")
-        client.subscribe(path)
 
-def disconnected(client):
-    # Disconnected function will be called when the client disconnects.
-    print('Disconnected from Adafruit IO!')
-    sys.exit(1)
+class RemoteRelays:
+    
+    def __init__(self):
+        self.client = mqtt_client.Client(CLIENT_ID, transport='websockets')
+        self.thread = threading.Thread(target=self.client.loop_forever())
+        self.thread.start()
 
-def message(client, feed_id, payload):
-    # Message function will be called when a subscribed feed has a new value.
-    # The feed_id parameter identifies the feed, and the payload parameter has
-    # the new value.
-    print('Feed {0} received new value: {1}'.format(feed_id, payload))
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print(f"Connected to MQTT Broker {MQTT_BROKER}!")
+            for feed, path in feeds.items():
+                print(f"Subscribing to {feed} on {path}")
+                self.client.subscribe(path)
+        else:
+            print(f"Failed to connect to {MQTT_BROKER}, return code {rc}".format(rc=rc), )
+
+    def disconnect(self):
+        print('Disconnecting from host!')
+        self.client.disconnect()
+        self.client.loop_stop()
+
+    def on_message(self, client, userdata, msg):
+        print("Received `{payload}` from `{topic}` topic".format(
+            payload=msg.payload.decode(), topic=msg.topic))
+
+    def power_onoff(self, channel, onoff):
+        self.client._client.publish(feeds[f"ONOFF{channel}"], onoff)
+
+    def set_pulse_ms(self, ms):
+        self.client._client.publish(feeds[f"PULSE_MS"], f"{ms}")
+
+    def power_pulse(self, channel, ms=None):
+        if ms is not None:
+            self.set_pulse_ms(ms)
+        self.client._client.publish(feeds["PULSE{channel}"])
+
+    def connect(self):
+        
+        #cert = get_ca_pem(MQTT_BROKER, MQTT_PORT)
+        #ctx = ssl.context(cert)
+        #client.tls_set_context(ctx)
+        #Mandate TLS1.2
+        self.client.tls_set()
+        self.client.username_pw_set(MQTT_USERNAME, MQTT_KEY)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(MQTT_BROKER, MQTT_PORT)
+        return self.client
 
 
-def power_onoff(channel, onoff):
-    client._client.publish(feeds[f"ONOFF{channel}"], onoff)
-
-def set_pulse_ms(ms):
-    client._client.publish(feeds[f"PULSE_MS"], f"{ms}")
-
-def power_pulse(channel, ms=None):
-    if ms is not None:
-        set_pulse_ms(ms)
-    client._client.publish(feeds["PULSE{channel}"])
+rr = RemoteRelays()
 
 
-# Create an MQTT client instance.
-client = MQTTClient(AIO_USERNAME, AIO_KEY)
+class TestPattern(threading.Thread):
+    def __init__(self):
+        self.evt = threading.Event()
+        super().__init__()
+        self.start()
 
-# Setup the callback functions defined above.
-client.on_connect    = connected
-client.on_disconnect = disconnected
-client.on_message    = message
-
-# Connect to the Adafruit IO server.
-client.connect()
-
-client.loop_background()
-
-
-while True:
-    channel = random.choice([0,1])
-    value = random.choice([0,1])
-    print(f'Publishing {value} to onoff{channel}')
-    power_onoff(channel, value)
-    time.sleep(5)
+    def run(self):
+        while True:
+            if rr.client.is_connected:
+                channel = random.choice([0,1])
+                value = random.choice([0,1])
+                print(f'Publishing {value} to onoff{channel}')
+                rr.power_onoff(channel, value)
+            if self.evt.is_set():
+                break
+            time.sleep(5)
 
 
+
+
+
+if __name__ == '__main__':
+    tp = TestPattern()
+    rr.connect()
+
+    while True:
+        try:
+            time.sleep(10)
+        except InterruptedError:
+            tp.evt.set()
+            break
+    tp.join()
+    rr.disconnect()
+    
+        
